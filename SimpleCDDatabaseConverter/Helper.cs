5a8ad14c03eff3bc62c711f6273451f05bbc69ff
xlfdll <xlfdll@gmail.com>
2018-10-06 23:01:23 -0700

    Switched runtime platform from .NET Core 2 to .NET Framework 4.7.2 - Also switched SQLite library from Microsoft.Data.Sqlite to System.Data.SQLite

--- SimpleCDDatabaseConverter/Helper.cs
+++ SimpleCDDatabaseConverter/Helper.cs
@@ -2,124 +2,123 @@
 using System.IO;
 using System.Text;
 
-using Microsoft.Data.Sqlite;
+using System.Data.SQLite;
 
 namespace VeryCDOfflineWebService
 {
-	public static class Helper
-	{
-		public static void Convert(String sourceFileName, String targetFileName)
-		{
-			SqliteConnectionStringBuilder sourceConnectionBuilder = new SqliteConnectionStringBuilder()
-			{
-				DataSource = sourceFileName,
-				Mode = SqliteOpenMode.ReadOnly
-			};
-			SqliteConnectionStringBuilder targetConnectionBuilder = new SqliteConnectionStringBuilder()
-			{
-				DataSource = targetFileName,
-				Mode = SqliteOpenMode.ReadWriteCreate
-			};
+    public static class Helper
+    {
+        public static void Convert(String sourceFileName, String targetFileName)
+        {
+            SQLiteConnectionStringBuilder sourceConnectionBuilder = new SQLiteConnectionStringBuilder()
+            {
+                DataSource = sourceFileName,
+                ReadOnly = true
+            };
+            SQLiteConnectionStringBuilder targetConnectionBuilder = new SQLiteConnectionStringBuilder()
+            {
+                DataSource = targetFileName
+            };
 
-			using (SqliteConnection sourceConnection = new SqliteConnection(sourceConnectionBuilder.ToString()))
-			{
-				using (SqliteConnection targetConnection = new SqliteConnection(targetConnectionBuilder.ToString()))
-				{
-					sourceConnection.Open();
-					targetConnection.Open();
+            using (SQLiteConnection sourceConnection = new SQLiteConnection(sourceConnectionBuilder.ToString()))
+            {
+                using (SQLiteConnection targetConnection = new SQLiteConnection(targetConnectionBuilder.ToString()))
+                {
+                    sourceConnection.Open();
+                    targetConnection.Open();
 
-					using (SqliteCommand createTableCommand = new SqliteCommand(Helper.SQL.CreateTable, targetConnection))
-					{
-						createTableCommand.ExecuteNonQuery();
-					}
+                    using (SQLiteCommand createTableCommand = new SQLiteCommand(Helper.SQL.CreateTable, targetConnection))
+                    {
+                        createTableCommand.ExecuteNonQuery();
+                    }
 
-					using (SqliteCommand selectCommand = new SqliteCommand(Helper.SQL.SelectAll, sourceConnection))
-					{
-						using (SqliteDataReader reader = selectCommand.ExecuteReader())
-						{
-							Int32 count = 0;
-							SqliteTransaction transaction = null;
+                    using (SQLiteCommand selectCommand = new SQLiteCommand(Helper.SQL.SelectAll, sourceConnection))
+                    {
+                        using (SQLiteDataReader reader = selectCommand.ExecuteReader())
+                        {
+                            Int32 count = 0;
+                            SQLiteTransaction transaction = null;
 
-							while (reader.Read())
-							{
-								if (count == 0)
-								{
-									transaction = targetConnection.BeginTransaction();
-								}
+                            while (reader.Read())
+                            {
+                                if (count == 0)
+                                {
+                                    transaction = targetConnection.BeginTransaction();
+                                }
 
-								using (SqliteCommand insertCommand = new SqliteCommand(Helper.SQL.InsertEntry, targetConnection, transaction))
-								{
-									try
-									{
-										insertCommand.Parameters.AddWithValue("@Title", reader["title"]);
-										insertCommand.Parameters.AddWithValue("@Description", $"{reader["brief"]}{Environment.NewLine}{Environment.NewLine}{reader["content"]}");
+                                using (SQLiteCommand insertCommand = new SQLiteCommand(Helper.SQL.InsertEntry, targetConnection, transaction))
+                                {
+                                    try
+                                    {
+                                        insertCommand.Parameters.AddWithValue("@Title", reader["title"]);
+                                        insertCommand.Parameters.AddWithValue("@Description", $"{reader["brief"]}{Environment.NewLine}{Environment.NewLine}{reader["content"]}");
 
 
-										String[] links = reader["ed2k"].ToString()
-											.Replace("", Environment.NewLine)
-											.Split(new String[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+                                        String[] links = reader["ed2k"].ToString()
+                                            .Replace("", Environment.NewLine)
+                                            .Split(new String[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
 
-										StringBuilder sb = new StringBuilder();
+                                        StringBuilder sb = new StringBuilder();
 
-										for (Int32 i = 0; i < links.Length; i += 2)
-										{
-											sb.AppendLine(links[i]);
-										}
+                                        for (Int32 i = 0; i < links.Length; i += 2)
+                                        {
+                                            sb.AppendLine(links[i]);
+                                        }
 
-										insertCommand.Parameters.AddWithValue("@Link", sb.ToString());
-										insertCommand.Parameters.AddWithValue("@Category", reader["category1"]);
-										insertCommand.Parameters.AddWithValue("@SubCategory", reader["category2"]);
-										insertCommand.Parameters.AddWithValue("@PublishTime", DateTime.Parse(reader["pubtime"].ToString()).ToString(Helper.DateTimeFormat));
-										insertCommand.Parameters.AddWithValue("@UpdateTime", DateTime.Parse(reader["updtime"].ToString()).ToString(Helper.DateTimeFormat));
+                                        insertCommand.Parameters.AddWithValue("@Link", sb.ToString());
+                                        insertCommand.Parameters.AddWithValue("@Category", reader["category1"]);
+                                        insertCommand.Parameters.AddWithValue("@SubCategory", reader["category2"]);
+                                        insertCommand.Parameters.AddWithValue("@PublishTime", DateTime.Parse(reader["pubtime"].ToString()).ToString(Helper.DateTimeFormat));
+                                        insertCommand.Parameters.AddWithValue("@UpdateTime", DateTime.Parse(reader["updtime"].ToString()).ToString(Helper.DateTimeFormat));
 
-										insertCommand.ExecuteNonQuery();
-									}
-									catch
-									{
-										String errorLine = $"! - {reader[0]}: {reader["title"]}";
+                                        insertCommand.ExecuteNonQuery();
+                                    }
+                                    catch
+                                    {
+                                        String errorLine = $"! - {reader[0]}: {reader["title"]}";
 
-										File.AppendAllText(targetFileName + ".log", errorLine + Environment.NewLine);
-										Console.WriteLine(errorLine);
-									}
-								}
+                                        File.AppendAllText(targetFileName + ".log", errorLine + Environment.NewLine);
+                                        Console.WriteLine(errorLine);
+                                    }
+                                }
 
-								count++;
+                                count++;
 
-								if (count == Helper.BatchSize)
-								{
-									transaction.Commit();
-									transaction.Dispose();
+                                if (count == Helper.BatchSize)
+                                {
+                                    transaction.Commit();
+                                    transaction.Dispose();
 
-									count = 0;
-								}
-							}
+                                    count = 0;
+                                }
+                            }
 
-							if (count != 0)
-							{
-								transaction.Commit();
-								transaction.Dispose();
-							}
+                            if (count != 0)
+                            {
+                                transaction.Commit();
+                                transaction.Dispose();
+                            }
 
-							using (SqliteCommand indexCommand = new SqliteCommand(Helper.SQL.CreateIndex, targetConnection))
-							{
-								indexCommand.ExecuteNonQuery();
-							}
-						}
-					}
-				}
-			}
-		}
+                            using (SQLiteCommand indexCommand = new SQLiteCommand(Helper.SQL.CreateIndex, targetConnection))
+                            {
+                                indexCommand.ExecuteNonQuery();
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
-		public const Int32 BatchSize = 1000;
+        public const Int32 BatchSize = 1000;
 
-		public static readonly String DateTimeFormat = "yyyy-MM-dd HH:mm:ss.FFFFFFF";
+        public static readonly String DateTimeFormat = "yyyy-MM-dd HH:mm:ss.FFFFFFF";
 
-		public static class SQL
-		{
-			public static readonly String CreateTable = @"CREATE TABLE IF NOT EXISTS Entries (ID INTEGER PRIMARY KEY, Title TEXT NOT NULL, Description TEXT NOT NULL, Link TEXT NOT NULL, Category TEXT NOT NULL, SubCategory TEXT NOT NULL, PublishTime Text NOT NULL, UpdateTime Text NOT NULL)";
-			public static readonly String SelectAll = @"SELECT * FROM verycd";
-			public static readonly String InsertEntry = @"INSERT INTO Entries VALUES (NULL, @Title, @Description, @Link, @Category, @SubCategory, @PublishTime, @UpdateTime)";
-			public static readonly String CreateIndex = @"CREATE INDEX TitleIndex ON Entries (Title)";
-		}
-	}
+        public static class SQL
+        {
+            public static readonly String CreateTable = @"CREATE TABLE IF NOT EXISTS Entries (ID INTEGER PRIMARY KEY, Title TEXT NOT NULL, Description TEXT NOT NULL, Link TEXT NOT NULL, Category TEXT NOT NULL, SubCategory TEXT NOT NULL, PublishTime Text NOT NULL, UpdateTime Text NOT NULL)";
+            public static readonly String SelectAll = @"SELECT * FROM verycd";
+            public static readonly String InsertEntry = @"INSERT INTO Entries VALUES (NULL, @Title, @Description, @Link, @Category, @SubCategory, @PublishTime, @UpdateTime)";
+            public static readonly String CreateIndex = @"CREATE INDEX TitleIndex ON Entries (Title)";
+        }
+    }
 }
