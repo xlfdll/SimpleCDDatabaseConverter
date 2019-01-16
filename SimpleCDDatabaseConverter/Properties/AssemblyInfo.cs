674f55202436d2a1c0fd156d65ca66f1cde8e06e
xlfdll <xlfdll@gmail.com>
2019-01-15 22:01:33 -0800

    Added Git build number generation script to use deterministic version representation

--- SimpleCDDatabaseConverter/Properties/AssemblyInfo.cs
+++ SimpleCDDatabaseConverter/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
+using Xlfdll;
+
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
@@ -31,4 +33,4 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0." + BuildInfo.Build)]
