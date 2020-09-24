7f78adb4f54830311adf4d202a83ac4b114c4675
xlfdll <xlfdll@gmail.com>
2020-09-23 21:04:12 -0700

    Added exception handler to properly display errors if occurred

--- SimpleCDDatabaseConverter/Program.cs
+++ SimpleCDDatabaseConverter/Program.cs
@@ -1,20 +1,21 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace VeryCDOfflineWebService
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine();
-			Console.WriteLine("SimpleCD Database Converter");
-			Console.WriteLine("(C) 2017 Xlfdll Workstation");
-			Console.WriteLine();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("SimpleCD Database Converter");
+            Console.WriteLine("(C) 2017 Xlfdll Workstation");
+            Console.WriteLine();
 
-			if (args.Length < 2)
-			{
-				Console.WriteLine("Usage: SimpleCDDatabaseConverter <source database file> <target database file>");
-				Console.WriteLine();
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: SimpleCDDatabaseConverter <source database file> <target database file>");
+                Console.WriteLine();
                 Console.WriteLine("<source database file>");
                 Console.WriteLine("- The file name of SimpleCD Desktop SQLite database to be converted from");
                 Console.WriteLine();
@@ -22,18 +23,27 @@ namespace VeryCDOfflineWebService
                 Console.WriteLine("- The file name of VeryCD Offline Web Service SQLite database to be converted to");
                 Console.WriteLine();
             }
-			else
-			{
-				Console.WriteLine("Converting SimpleCD database ...");
-				Console.WriteLine($"- From: {args[0]}");
-				Console.WriteLine($"- To: {args[1]}");
-				Console.WriteLine();
+            else
+            {
+                Console.WriteLine("Converting SimpleCD database ...");
+                Console.WriteLine($"- From: {args[0]}");
+                Console.WriteLine($"- To: {args[1]}");
+                Console.WriteLine();
 
-				Helper.Convert(args[0], args[1]);
+                try
+                {
+                    Helper.Convert(args[0], args[1]);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"ERROR: {ex.Message}");
 
-				Console.WriteLine("Done.");
-				Console.WriteLine();
-			}
-		}
-	}
+                    Trace.WriteLine(ex);
+                }
+
+                Console.WriteLine("Done.");
+                Console.WriteLine();
+            }
+        }
+    }
 }
