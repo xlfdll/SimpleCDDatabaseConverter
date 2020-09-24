335d104dc44d83257f0b8ade85622b44c99906f6
xlfdll <xlfdll@gmail.com>
2020-09-23 21:51:48 -0700

    Added code to build full-text search virtual table

--- SimpleCDDatabaseConverter/Helper.cs
+++ SimpleCDDatabaseConverter/Helper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 
@@ -10,6 +11,15 @@ namespace VeryCDOfflineWebService
     {
         public static void Convert(String sourceFileName, String targetFileName)
         {
+            if (File.Exists(targetFileName))
+            {
+                File.Delete(targetFileName);
+            }
+            if (File.Exists(targetFileName + ".log"))
+            {
+                File.Delete(targetFileName + ".log");
+            }
+
             SQLiteConnectionStringBuilder sourceConnectionBuilder = new SQLiteConnectionStringBuilder()
             {
                 DataSource = sourceFileName,
@@ -27,98 +37,148 @@ namespace VeryCDOfflineWebService
                     sourceConnection.Open();
                     targetConnection.Open();
 
-                    using (SQLiteCommand createTableCommand = new SQLiteCommand(Helper.SQL.CreateTable, targetConnection))
-                    {
-                        createTableCommand.ExecuteNonQuery();
-                    }
+                    targetConnection.EnableExtensions(true);
+                    targetConnection.LoadExtension("SQLite.Interop.dll", "sqlite3_fts5_init");
 
-                    using (SQLiteCommand selectCommand = new SQLiteCommand(Helper.SQL.SelectAll, sourceConnection))
+                    Helper.CreateDataSchema(targetConnection);
+                    Helper.FillDataEntries(targetFileName, sourceConnection, targetConnection);
+                }
+            }
+        }
+
+        private static void CreateDataSchema(SQLiteConnection targetConnection)
+        {
+            using (SQLiteCommand createTableCommand = new SQLiteCommand(Helper.SQL.CreateTable, targetConnection))
+            {
+                createTableCommand.ExecuteNonQuery();
+            }
+
+            using (SQLiteCommand createFTSTableCommand = new SQLiteCommand(Helper.SQL.CreateFTSTable, targetConnection))
+            {
+                createFTSTableCommand.ExecuteNonQuery();
+            }
+
+            using (SQLiteCommand createFTSInsertTriggerCommand = new SQLiteCommand(Helper.SQL.CreateFTSInsertTrigger, targetConnection))
+            {
+                createFTSInsertTriggerCommand.ExecuteNonQuery();
+            }
+
+            using (SQLiteCommand createFTSUpdateTriggerCommand = new SQLiteCommand(Helper.SQL.CreateFTSUpdateTrigger, targetConnection))
+            {
+                createFTSUpdateTriggerCommand.ExecuteNonQuery();
+            }
+
+            using (SQLiteCommand createFTSDeleteTriggerCommand = new SQLiteCommand(Helper.SQL.CreateFTSDeleteTrigger, targetConnection))
+            {
+                createFTSDeleteTriggerCommand.ExecuteNonQuery();
+            }
+        }
+
+        private static void FillDataEntries(string targetFileName, SQLiteConnection sourceConnection, SQLiteConnection targetConnection)
+        {
+            using (SQLiteCommand selectCommand = new SQLiteCommand(Helper.SQL.SelectAll, sourceConnection))
+            {
+                using (SQLiteDataReader reader = selectCommand.ExecuteReader())
+                {
+                    Int32 count = 0;
+                    SQLiteTransaction transaction = null;
+
+                    while (reader.Read())
                     {
-                        using (SQLiteDataReader reader = selectCommand.ExecuteReader())
+                        if (count == 0)
                         {
-                            Int32 count = 0;
-                            SQLiteTransaction transaction = null;
+                            transaction = targetConnection.BeginTransaction();
+                        }
 
-                            while (reader.Read())
+                        using (SQLiteCommand insertCommand = new SQLiteCommand(Helper.SQL.InsertEntry, targetConnection, transaction))
+                        {
+                            try
                             {
-                                if (count == 0)
+                                insertCommand.Parameters.AddWithValue("@Title", reader["title"]);
+                                insertCommand.Parameters.AddWithValue("@Description", $"{reader["brief"]}{Environment.NewLine}{Environment.NewLine}{reader["content"]}");
+
+                                String[] links = reader["ed2k"].ToString()
+                                    .Replace("`", Environment.NewLine)
+                                    .Split(new String[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+                                StringBuilder sb = new StringBuilder();
+
+                                for (Int32 i = 0; i < links.Length; i += 2)
                                 {
-                                    transaction = targetConnection.BeginTransaction();
+                                    sb.AppendLine(links[i]);
                                 }
 
-                                using (SQLiteCommand insertCommand = new SQLiteCommand(Helper.SQL.InsertEntry, targetConnection, transaction))
-                                {
-                                    try
-                                    {
-                                        insertCommand.Parameters.AddWithValue("@Title", reader["title"]);
-                                        insertCommand.Parameters.AddWithValue("@Description", $"{reader["brief"]}{Environment.NewLine}{Environment.NewLine}{reader["content"]}");
+                                insertCommand.Parameters.AddWithValue("@Link", sb.ToString());
+                                insertCommand.Parameters.AddWithValue("@Category", reader["category1"]);
+                                insertCommand.Parameters.AddWithValue("@SubCategory", reader["category2"]);
+                                insertCommand.Parameters.AddWithValue("@PublishTime", DateTime.Parse(reader["pubtime"].ToString()).ToString(Helper.DateTimeFormat));
+                                insertCommand.Parameters.AddWithValue("@UpdateTime", DateTime.Parse(reader["updtime"].ToString()).ToString(Helper.DateTimeFormat));
 
-
-                                        String[] links = reader["ed2k"].ToString()
-                                            .Replace("", Environment.NewLine)
-                                            .Split(new String[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
-
-                                        StringBuilder sb = new StringBuilder();
-
-                                        for (Int32 i = 0; i < links.Length; i += 2)
-                                        {
-                                            sb.AppendLine(links[i]);
-                                        }
-
-                                        insertCommand.Parameters.AddWithValue("@Link", sb.ToString());
-                                        insertCommand.Parameters.AddWithValue("@Category", reader["category1"]);
-                                        insertCommand.Parameters.AddWithValue("@SubCategory", reader["category2"]);
-                                        insertCommand.Parameters.AddWithValue("@PublishTime", DateTime.Parse(reader["pubtime"].ToString()).ToString(Helper.DateTimeFormat));
-                                        insertCommand.Parameters.AddWithValue("@UpdateTime", DateTime.Parse(reader["updtime"].ToString()).ToString(Helper.DateTimeFormat));
-
-                                        insertCommand.ExecuteNonQuery();
-                                    }
-                                    catch
-                                    {
-                                        String errorLine = $"! - {reader[0]}: {reader["title"]}";
-
-                                        File.AppendAllText(targetFileName + ".log", errorLine + Environment.NewLine);
-                                        Console.WriteLine(errorLine);
-                                    }
-                                }
-
-                                count++;
-
-                                if (count == Helper.BatchSize)
-                                {
-                                    transaction.Commit();
-                                    transaction.Dispose();
-
-                                    count = 0;
-                                }
+                                insertCommand.ExecuteNonQuery();
                             }
-
-                            if (count != 0)
+                            catch (Exception ex)
                             {
-                                transaction.Commit();
-                                transaction.Dispose();
-                            }
+                                String errorLine = $"! - {reader[0]}: {reader["title"]}";
 
-                            using (SQLiteCommand indexCommand = new SQLiteCommand(Helper.SQL.CreateIndex, targetConnection))
-                            {
-                                indexCommand.ExecuteNonQuery();
+                                File.AppendAllText(targetFileName + ".log", errorLine + Environment.NewLine);
+                                Console.WriteLine(errorLine);
+
+                                Trace.WriteLine(ex);
                             }
                         }
+
+                        count++;
+
+                        if (count == Helper.BatchSize)
+                        {
+                            transaction.Commit();
+                            transaction.Dispose();
+
+                            count = 0;
+                        }
+                    }
+
+                    if (count != 0)
+                    {
+                        transaction.Commit();
+                        transaction.Dispose();
+                    }
+
+                    using (SQLiteCommand indexCommand = new SQLiteCommand(Helper.SQL.CreateIndex, targetConnection))
+                    {
+                        indexCommand.ExecuteNonQuery();
                     }
                 }
             }
         }
 
-        public const Int32 BatchSize = 1000;
-
+        public const Int32 BatchSize = 10000;
         public const String DateTimeFormat = "yyyy-MM-dd HH:mm:ss.FFFFFFF";
 
         public static class SQL
         {
-            public const String CreateTable = @"CREATE TABLE IF NOT EXISTS Entries (ID INTEGER PRIMARY KEY, Title TEXT NOT NULL, Description TEXT NOT NULL, Link TEXT NOT NULL, Category TEXT NOT NULL, SubCategory TEXT NOT NULL, PublishTime Text NOT NULL, UpdateTime Text NOT NULL)";
-            public const String SelectAll = @"SELECT * FROM verycd";
-            public const String InsertEntry = @"INSERT INTO Entries VALUES (NULL, @Title, @Description, @Link, @Category, @SubCategory, @PublishTime, @UpdateTime)";
-            public const String CreateIndex = @"CREATE INDEX TitleIndex ON Entries (Title)";
+            public const String CreateTable = "CREATE TABLE IF NOT EXISTS Entries (ID INTEGER PRIMARY KEY, Title TEXT NOT NULL, Description TEXT NOT NULL, Link TEXT NOT NULL, Category TEXT NOT NULL, SubCategory TEXT NOT NULL, PublishTime Text NOT NULL, UpdateTime Text NOT NULL);";
+            public const String SelectAll = "SELECT * FROM verycd;";
+            public const String InsertEntry = "INSERT INTO Entries VALUES (NULL, @Title, @Description, @Link, @Category, @SubCategory, @PublishTime, @UpdateTime);";
+            public const String CreateIndex = "CREATE INDEX TitleIndex ON Entries (Title);";
+
+            public const String CreateFTSTable = "CREATE VIRTUAL TABLE Entries_FTS USING fts5(Title, Description, content='Entries', content_rowid='ID');";
+            public const String CreateFTSInsertTrigger
+                = "CREATE TRIGGER Entries_FTS_Insert_Trigger AFTER INSERT ON Entries"
+                + " BEGIN"
+                + " INSERT INTO Entries_FTS (rowid, Title, Description) VALUES (new.ID, new.Title, new.Description);"
+                + " END;";
+            public const String CreateFTSUpdateTrigger
+                = "CREATE TRIGGER Entries_FTS_Update_Trigger AFTER UPDATE ON Entries"
+                + " BEGIN"
+                + " INSERT INTO Entries_FTS (Entries_FTS, rowid, Title, Description) VALUES ('delete', old.ID, old.Title, old.Description);"
+                + " INSERT INTO Entries_FTS (rowid, Title, Description) VALUES (new.ID, new.Title, new.Description);"
+                + " END;";
+            public const String CreateFTSDeleteTrigger
+                = "CREATE TRIGGER Entries_FTS_Delete_Trigger AFTER DELETE ON Entries"
+                + " BEGIN"
+                + " INSERT INTO Entries_FTS (Entries_FTS, rowid, Title, Description) VALUES ('delete', old.ID, old.Title, old.Description);"
+                + " END;";
         }
     }
 }
